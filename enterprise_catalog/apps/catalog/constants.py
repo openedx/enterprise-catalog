6cd74161f57ecf896e07ec33dc3e5fc896b41417
Adam Stankiewicz <agstanki@gmail.com>
2019-12-23 14:15:37 -0500

    factory updates and move some things to constants.py

--- enterprise_catalog/apps/catalog/constants.py
+++ enterprise_catalog/apps/catalog/constants.py
@@ -1,6 +1,16 @@
 import json
 
 
+# ContentMetadata content_type choices
+COURSE = 'course'
+COURSE_RUN = 'course_run'
+PROGRAM = 'program'
+CONTENT_TYPE_CHOICES = [
+    (COURSE, 'Course'),
+    (COURSE_RUN, 'Course Run'),
+    (PROGRAM, 'Program'),
+]
+
 # Course mode sorting based on slug
 COURSE_MODE_SORT_ORDER = ['verified', 'professional', 'no-id-professional', 'audit', 'honor']
 
