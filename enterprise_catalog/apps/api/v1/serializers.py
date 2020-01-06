ad7287da2b9d84f239491ae550e675310b413948
Brandon Baker <bbaker@edx.org>
2020-01-06 14:52:28 -0500

    Allow writing uuid only on POST

--- enterprise_catalog/apps/api/v1/serializers.py
+++ enterprise_catalog/apps/api/v1/serializers.py
@@ -50,10 +50,8 @@ class CatalogContentKeySerializer(QuerySerializerMixin, serializers.ModelSeriali
 
 class EnterpriseCatalogSerializer(QuerySerializerMixin, serializers.ModelSerializer):
     """
-    Serializer for the `EnterpriseCatalogModel`
+    Serializer for the `EnterpriseCatalog` model
     """
-    # UUID should only be writable on POST
-    uuid = serializers.UUIDField(read_only=False)
     enterprise_customer = serializers.UUIDField(source='enterprise_uuid')
     # content_filter = This is in the mgmt command WIP, but not sure if it should be part of this serializer
     enabled_course_modes = serializers.JSONField(write_only=True)
@@ -62,3 +60,12 @@ class EnterpriseCatalogSerializer(QuerySerializerMixin, serializers.ModelSeriali
     class Meta:
         model = EnterpriseCatalog
         fields = ['uuid', 'title', 'enterprise_customer', 'enabled_course_modes', 'publish_audit_enrollment_urls']
+
+
+class EnterpriseCatalogCreateSerializer(EnterpriseCatalogSerializer):
+    """
+    Serializer for POST requests on the `EnterpriseCatalog` model
+
+    UUID is writable to allow importing existing Enterprise Catalogs and keeping the same UUID
+    """
+    uuid = serializers.UUIDField(read_only=False)
--- enterprise_catalog/apps/api/v1/views.py
+++ enterprise_catalog/apps/api/v1/views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets
 
-from enterprise_catalog.apps.api.v1.serializers import EnterpriseCatalogSerializer
+from enterprise_catalog.apps.api.v1.serializers import (
+    EnterpriseCatalogSerializer,
+    EnterpriseCatalogCreateSerializer,
+)
 from enterprise_catalog.apps.catalog.models import EnterpriseCatalog
 
 
@@ -8,3 +11,9 @@ class EnterpriseCatalogViewSet(viewsets.ModelViewSet):
     """ View for CRUD operations on Enterprise Catalogs """
     serializer_class = EnterpriseCatalogSerializer
     queryset = EnterpriseCatalog.objects.all()
+
+    def get_serializer_class(self):
+        action = getattr(self, 'action', None)
+        if action == 'create':
+            return EnterpriseCatalogCreateSerializer
+        return EnterpriseCatalogSerializer
