b137ceb2b72465cc8a92a1c245cb1f1574b1588c
Brandon Baker <bbaker@edx.org>
2020-01-07 15:59:09 -0500

    Remove unused serializers for now

--- enterprise_catalog/apps/api/v1/serializers.py
+++ enterprise_catalog/apps/api/v1/serializers.py
@@ -1,54 +1,9 @@
 from rest_framework import serializers
 
-from enterprise_catalog.apps.catalog.models import (
-    CatalogContentKey,
-    CatalogQuery,
-    EnterpriseCatalog,
-)
+from enterprise_catalog.apps.catalog.models import EnterpriseCatalog
 
 
-class QuerySerializerMixin:
-    """
-    Mixin for DRY query improvements. TBD if we need this or not
-
-    Used by inheriting from the mixin and declaring the `related_fields` and `prefetch_fields` specific to the model
-    on your serializer, and then calling `get_related_queries` when getting the queryset in your view.
-
-    Adapted from https://riptutorial.com/django-rest-framework/example/7832/speed-up-serializers-queries
-    """
-    related_fields = []  # For selecting a single object (i.e. OneToOne or ForeignKey)
-    prefetch_fields = []  # For selecting a group of objects (i.e. ManyToMany, reverse ForeignKeys, etc.)
-
-    @classmethod
-    def get_related_queries(cls, queryset):
-        if cls.related_fields:
-            queryset = queryset.select_related(*cls.related_fields)
-        if cls.prefetch_fields:
-            queryset = queryset.prefetch_related(*cls.prefetch_fields)
-
-        return queryset
-
-
-class CatalogQuerySerializer(QuerySerializerMixin, serializers.ModelSerializer):
-    """
-    Serializer for the `CatalogQuery` model
-    """
-
-    class Meta:
-        model = CatalogQuery
-        fields = ['id']
-
-class CatalogContentKeySerializer(QuerySerializerMixin, serializers.ModelSerializer):
-    """
-    Serializer for the `CatalogContentKey` model
-    """
-
-    class Meta:
-        model = CatalogContentKey
-        fields = ['id']
-
-
-class EnterpriseCatalogSerializer(QuerySerializerMixin, serializers.ModelSerializer):
+class EnterpriseCatalogSerializer(serializers.ModelSerializer):
     """
     Serializer for the `EnterpriseCatalog` model
     """
