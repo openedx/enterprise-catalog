87760a541e0bf43e71cf2918c8344d79b6b85b84
Brandon Baker <bbaker@edx.org>
2020-01-08 10:28:34 -0500

    Handle content_filter field

--- enterprise_catalog/apps/api/v1/serializers.py
+++ enterprise_catalog/apps/api/v1/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 
-from enterprise_catalog.apps.catalog.models import EnterpriseCatalog
+from enterprise_catalog.apps.catalog.models import (
+    CatalogQuery,
+    EnterpriseCatalog,
+)
+
+from enterprise_catalog.apps.catalog.utils import get_content_filter_hash
 
 
 class EnterpriseCatalogSerializer(serializers.ModelSerializer):
@@ -8,13 +13,25 @@ class EnterpriseCatalogSerializer(serializers.ModelSerializer):
     Serializer for the `EnterpriseCatalog` model
     """
     enterprise_customer = serializers.UUIDField(source='enterprise_uuid')
-    # content_filter = This is in the mgmt command WIP, but not sure if it should be part of this serializer
     enabled_course_modes = serializers.JSONField(write_only=True)
     publish_audit_enrollment_urls = serializers.BooleanField(write_only=True)
+    content_filter = serializers.JSONField(write_only=True)
 
     class Meta:
         model = EnterpriseCatalog
-        fields = ['uuid', 'title', 'enterprise_customer', 'enabled_course_modes', 'publish_audit_enrollment_urls']
+        fields = [
+            'uuid',
+            'title',
+            'enterprise_customer',
+            'enabled_course_modes',
+            'publish_audit_enrollment_urls',
+            'content_filter',
+        ]
+
+    def create(self, validated_data):
+        content_filter = validated_data.pop('content_filter')
+        catalog_query, _ = CatalogQuery.objects.get_or_create(content_filter_hash=get_content_filter_hash(content_filter))
+        return EnterpriseCatalog.objects.create(**validated_data, catalog_query=catalog_query)
 
 
 class EnterpriseCatalogCreateSerializer(EnterpriseCatalogSerializer):
@@ -23,4 +40,4 @@ class EnterpriseCatalogCreateSerializer(EnterpriseCatalogSerializer):
 
     UUID is writable to allow importing existing Enterprise Catalogs and keeping the same UUID
     """
-    uuid = serializers.UUIDField(read_only=False)
+    uuid = serializers.UUIDField(read_only=False, required=False)
