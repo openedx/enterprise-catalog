d9c103944f71a481abe771c9e2d6e19586aa81ad
Brandon Baker <bbaker@edx.org>
2020-01-10 08:08:05 -0500

    Update permissions, auth classes

--- enterprise_catalog/apps/api/v1/views.py
+++ enterprise_catalog/apps/api/v1/views.py
@@ -1,4 +1,7 @@
+from edx_rest_framework_extensions.auth.bearer.authentication import BearerAuthentication
+from edx_rest_framework_extensions.auth.jwt.authentication import JwtAuthentication
 from rest_framework import permissions, viewsets
+from rest_framework.authentication import SessionAuthentication
 
 from enterprise_catalog.apps.api.v1.serializers import (
     EnterpriseCatalogCreateSerializer,
@@ -11,7 +14,8 @@ class EnterpriseCatalogViewSet(viewsets.ModelViewSet):
     """ View for CRUD operations on Enterprise Catalogs """
     serializer_class = EnterpriseCatalogSerializer
     queryset = EnterpriseCatalog.objects.all().order_by('created')
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
+    authentication_classes = [JwtAuthentication, BearerAuthentication, SessionAuthentication]
     lookup_field = 'uuid'
 
     def get_serializer_class(self):
