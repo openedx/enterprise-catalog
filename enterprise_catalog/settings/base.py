cf47618e0d35ce41192ed4a74356a927538fd133
Brandon Baker <bbaker6225@users.noreply.github.com>
2020-01-14 13:24:29 -0500

    Change login redirect url to /api-docs/ (#37)

--- enterprise_catalog/settings/base.py
+++ enterprise_catalog/settings/base.py
@@ -217,8 +217,7 @@ JWT_AUTH = {
 # Request the user's permissions in the ID token
 EXTRA_SCOPE = ['permissions']
 
-# TODO Set this to another (non-staff, ideally) path.
-LOGIN_REDIRECT_URL = '/admin/'
+LOGIN_REDIRECT_URL = '/api-docs/'
 # END AUTHENTICATION CONFIGURATION
 
 
