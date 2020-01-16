040e76c2ba5b0d5685573a081ed758c0f217f330
Brandon Baker <bbaker6225@users.noreply.github.com>
2020-01-16 08:59:50 -0500

    Disable changing models through admin (#40)
    * Disable changing models through admin
    
    * Put disabling behind waffle switch

--- enterprise_catalog/apps/catalog/admin.py
+++ enterprise_catalog/apps/catalog/admin.py
@@ -4,6 +4,9 @@ from django.core.exceptions import ValidationError
 from django.urls import reverse
 from django.utils.html import format_html
 
+from enterprise_catalog.apps.catalog.constants import (
+    admin_model_changes_allowed,
+)
 from enterprise_catalog.apps.catalog.models import (
     CatalogQuery,
     ContentMetadata,
@@ -12,8 +15,31 @@ from enterprise_catalog.apps.catalog.models import (
 from enterprise_catalog.apps.catalog.utils import get_content_filter_hash
 
 
+class UnchangeableMixin(admin.ModelAdmin):
+    """
+    Mixin for disabling changing models through the admin
+
+    We're disabling changing models in this admin while we transition over from the LMS
+    """
+    @classmethod
+    def has_add_permission(cls, request):
+        return admin_model_changes_allowed()
+
+    @classmethod
+    def has_delete_permission(cls, request, obj=None):
+        return admin_model_changes_allowed()
+
+    def changeform_view(self, request, object_id=None, form_url='', extra_context=None):
+        extra_context = extra_context or {}
+        if not admin_model_changes_allowed():
+            extra_context['show_save_and_continue'] = False
+            extra_context['show_save'] = False
+
+        return super().changeform_view(request, object_id, extra_context=extra_context)
+
+
 @admin.register(ContentMetadata)
-class ContentMetadataAdmin(admin.ModelAdmin):
+class ContentMetadataAdmin(UnchangeableMixin):
     """ Admin configuration for the custom ContentMetadata model. """
     list_display = ('id', 'content_key', 'content_type',)
 
@@ -32,14 +58,14 @@ class CatalogQueryForm(forms.ModelForm):
 
 
 @admin.register(CatalogQuery)
-class CatalogQueryAdmin(admin.ModelAdmin):
+class CatalogQueryAdmin(UnchangeableMixin):
     """ Admin configuration for the custom CatalogQuery model. """
     list_display = ('id',)
     form = CatalogQueryForm
 
 
 @admin.register(EnterpriseCatalog)
-class EnterpriseCatalogAdmin(admin.ModelAdmin):
+class EnterpriseCatalogAdmin(UnchangeableMixin):
     """ Admin configuration for the custom EnterpriseCatalog model. """
     list_display = ('uuid', 'enterprise_uuid', 'title', 'get_catalog_query',)
 
