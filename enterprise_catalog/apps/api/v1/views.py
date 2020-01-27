1ad6df3d2f478dfb1faed29d09255e79350d1c18
Brandon Baker <bbaker6225@users.noreply.github.com>
2020-01-27 08:12:15 -0500

    Add get_content_metadata endpoint on catalogs (#49)
    ENT-2563

--- enterprise_catalog/apps/api/v1/views.py
+++ enterprise_catalog/apps/api/v1/views.py
@@ -8,7 +8,9 @@ from edx_rest_framework_extensions.auth.jwt.authentication import (
 from rest_framework import permissions, viewsets
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.decorators import action
+from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
+from rest_framework_xml.renderers import XMLRenderer
 from six.moves.urllib.parse import quote_plus, unquote
 
 from enterprise_catalog.apps.api.v1.decorators import (
@@ -18,7 +20,10 @@ from enterprise_catalog.apps.api.v1.serializers import (
     EnterpriseCatalogCreateSerializer,
     EnterpriseCatalogSerializer,
 )
-from enterprise_catalog.apps.catalog.models import EnterpriseCatalog
+from enterprise_catalog.apps.catalog.models import (
+    ContentMetadata,
+    EnterpriseCatalog,
+)
 
 
 class EnterpriseCatalogViewSet(viewsets.ModelViewSet):
@@ -26,6 +31,7 @@ class EnterpriseCatalogViewSet(viewsets.ModelViewSet):
     queryset = EnterpriseCatalog.objects.all().order_by('created')
     permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser]
     authentication_classes = [JwtAuthentication, BearerAuthentication, SessionAuthentication]
+    renderer_classes = [JSONRenderer, XMLRenderer]
     lookup_field = 'uuid'
 
     def get_serializer_class(self):
@@ -37,7 +43,7 @@ class EnterpriseCatalogViewSet(viewsets.ModelViewSet):
 
     @method_decorator(require_at_least_one_query_parameter('course_run_ids', 'program_uuids'))
     @action(detail=True)
-    def contains_content_items(self, request, uuid, course_run_ids, program_uuids):
+    def contains_content_items(self, request, uuid, course_run_ids, program_uuids, **kwargs):
         """
         Returns whether or not the EnterpriseCatalog contains the specified content.
 
@@ -50,3 +56,30 @@ class EnterpriseCatalogViewSet(viewsets.ModelViewSet):
         enterprise_catalog = self.get_object()
         contains_content_items = enterprise_catalog.contains_content_keys(course_run_ids + program_uuids)
         return Response({'contains_content_items': contains_content_items})
+
+    @action(detail=True)
+    def get_content_metadata(self, request, uuid, **kwargs):
+        """
+        Returns all the content linked to the specified catalog, ordered by content key.
+        """
+        enterprise_catalog = self.get_object()
+        metadata = {
+            'uuid': enterprise_catalog.uuid,
+            'title': enterprise_catalog.title,
+            'enterprise_customer': enterprise_catalog.enterprise_uuid,
+            'count': 0,
+            'previous': None, 'next': None,  # Kept for parity with edx-enterprise
+            'results': [],
+        }
+
+        catalog_query = enterprise_catalog.catalog_query
+        if not catalog_query:
+            return Response(metadata)
+
+        associated_metadata = catalog_query.contentmetadata_set.all()
+        sorted_content_keys = sorted([metadata_chunk.content_key for metadata_chunk in associated_metadata])
+        metadata['results'] = [ContentMetadata.objects.get(content_key=content_key).json_metadata for content_key
+                               in sorted_content_keys]
+        metadata['count'] = len(sorted_content_keys)
+
+        return Response(metadata)
