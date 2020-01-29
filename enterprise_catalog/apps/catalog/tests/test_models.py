6e1fd6091297ccf447b4b316478b61de5b507065
Brandon Baker <bbaker@edx.org>
2020-01-29 14:37:29 -0500

    Set content_type and parent_content_key
    ENT-2585

--- enterprise_catalog/apps/catalog/tests/test_models.py
+++ enterprise_catalog/apps/catalog/tests/test_models.py
@@ -5,6 +5,11 @@ from collections import OrderedDict
 import mock
 from django.test import TestCase
 
+from enterprise_catalog.apps.catalog.constants import (
+    COURSE,
+    COURSE_RUN,
+    PROGRAM,
+)
 from enterprise_catalog.apps.catalog.models import (
     ContentMetadata,
     update_contentmetadata_from_discovery,
@@ -15,58 +20,52 @@ from enterprise_catalog.apps.catalog.tests import factories
 class TestModels(TestCase):
     """ Models tests. """
 
-    def _get_content_key(self, metadata):
-        """
-        Helper to get the content key, as it can be stored under either `key` or `uuid`.
-        """
-        return metadata.get('key') or metadata.get('uuid')
-
     @mock.patch('enterprise_catalog.apps.catalog.models.DiscoveryApiClient')
     def test_contentmetadata_update_from_discovery(self, mock_client):
         """
         update_contentmetadata_from_discovery should update or create
         ContentMetadata Objects from the discovery service api call/
         """
-        metadata_list = [
-            OrderedDict([('key', 'course-v1:my+course+1'), ('title', 'course 1')]),
-            OrderedDict([('key', 'course-v1:my+course+2'), ('title', 'course 2')]),
-            OrderedDict([('key', 'course-v1:my+course+3'), ('title', 'course 3')]),
-            OrderedDict([('uuid', 'fake-uuid'), ('title', 'program 1')]),
-        ]
+        course_metadata = OrderedDict([
+            ('aggregation_key', 'course:edX+testX'),
+            ('key', 'edX+testX'),
+            ('title', 'test course'),
+        ])
+        course_run_metadata = OrderedDict([
+            ('aggregation_key', 'courserun:edX+testX'),
+            ('key', 'course-v1:edX+testX+1'),
+            ('title', 'test course run'),
+        ])
+        program_metadata = OrderedDict([
+            ('aggregation_key', 'program:fake-uuid'),
+            ('title', 'test program'),
+            ('uuid', 'fake-uuid'),
+        ])
         mock_client.return_value.get_metadata_by_query.return_value = {
-            'count': 4,
+            'count': 3,
             'previous': None,
             'next': None,
-            'results': [
-                {
-                    'key': metadata_list[0]['key'],
-                    'title': metadata_list[0]['title'],
-                },
-                {
-                    'key': metadata_list[1]['key'],
-                    'title': metadata_list[1]['title'],
-                },
-                {
-                    'key': metadata_list[2]['key'],
-                    'title': metadata_list[2]['title'],
-                },
-                {
-                    'uuid': metadata_list[3]['uuid'],
-                    'title': metadata_list[3]['title'],
-                },
-            ]
+            'results': [course_metadata, course_run_metadata, program_metadata],
         }
         catalog = factories.EnterpriseCatalogFactory()
 
         self.assertEqual(ContentMetadata.objects.count(), 0)
         update_contentmetadata_from_discovery(catalog.uuid)
         mock_client.assert_called_once()
-        self.assertEqual(ContentMetadata.objects.count(), 4)
+        self.assertEqual(ContentMetadata.objects.count(), 3)
 
-        # Assert values in json_metadata are correct
-        for metadata in metadata_list:
-            metadata_key = self._get_content_key(metadata)
-            cm = ContentMetadata.objects.get(content_key=metadata_key)
-            json_metadata_key = self._get_content_key(cm.json_metadata)
-            self.assertEqual(json_metadata_key, metadata_key)
-            self.assertEqual(cm.json_metadata['title'], metadata['title'])
+        # Assert stored content metadata is correct for each type
+        course_cm = ContentMetadata.objects.get(content_key=course_metadata['key'])
+        self.assertEqual(course_cm.content_type, COURSE)
+        self.assertEqual(course_cm.parent_content_key, None)
+        self.assertEqual(course_cm.json_metadata, course_metadata)
+
+        course_run_cm = ContentMetadata.objects.get(content_key=course_run_metadata['key'])
+        self.assertEqual(course_run_cm.content_type, COURSE_RUN)
+        self.assertEqual(course_run_cm.parent_content_key, course_metadata['key'])
+        self.assertEqual(course_run_cm.json_metadata, course_run_metadata)
+
+        program_cm = ContentMetadata.objects.get(content_key=program_metadata['uuid'])
+        self.assertEqual(program_cm.content_type, PROGRAM)
+        self.assertEqual(program_cm.parent_content_key, None)
+        self.assertEqual(program_cm.json_metadata, program_metadata)
