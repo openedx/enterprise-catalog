6a803713798abffb0916068e9c34a1e49c9b29bc
Chris Pappas <christopappas@users.noreply.github.com>
2020-02-10 16:02:43 -0500

    ENT-2608 | Adding logic to metadata update function to associate Cont… (#59)
    * ENT-2608 | Adding logic to metadata update function to associate ContentMetadata objects with their catalog query
    
    * quality fixes
    
    * feedback from review

--- enterprise_catalog/apps/catalog/tests/test_models.py
+++ enterprise_catalog/apps/catalog/tests/test_models.py
@@ -41,12 +41,19 @@ class TestModels(TestCase):
             ('title', 'test program'),
             ('uuid', 'fake-uuid'),
         ])
-        mock_client.return_value.get_metadata_by_query.return_value = {
-            'count': 3,
-            'previous': None,
-            'next': None,
-            'results': [course_metadata, course_run_metadata, program_metadata],
-        }
+        mock_client.return_value.get_metadata_by_query.side_effect = [{
+                'count': 3,
+                'previous': None,
+                'next': None,
+                'results': [course_metadata, course_run_metadata, program_metadata],
+            },
+            {
+                'count': 1,
+                'previous': None,
+                'next': None,
+                'results': [program_metadata],
+            }
+        ]
         catalog = factories.EnterpriseCatalogFactory()
 
         self.assertEqual(ContentMetadata.objects.count(), 0)
@@ -54,18 +61,34 @@ class TestModels(TestCase):
         mock_client.assert_called_once()
         self.assertEqual(ContentMetadata.objects.count(), 3)
 
+        associated_metadata = catalog.catalog_query.contentmetadata_set.all()
+
         # Assert stored content metadata is correct for each type
         course_cm = ContentMetadata.objects.get(content_key=course_metadata['key'])
         self.assertEqual(course_cm.content_type, COURSE)
         self.assertEqual(course_cm.parent_content_key, None)
         self.assertEqual(course_cm.json_metadata, course_metadata)
+        assert course_cm in associated_metadata
 
         course_run_cm = ContentMetadata.objects.get(content_key=course_run_metadata['key'])
         self.assertEqual(course_run_cm.content_type, COURSE_RUN)
         self.assertEqual(course_run_cm.parent_content_key, course_metadata['key'])
         self.assertEqual(course_run_cm.json_metadata, course_run_metadata)
+        assert course_run_cm in associated_metadata
 
         program_cm = ContentMetadata.objects.get(content_key=program_metadata['uuid'])
         self.assertEqual(program_cm.content_type, PROGRAM)
         self.assertEqual(program_cm.parent_content_key, None)
         self.assertEqual(program_cm.json_metadata, program_metadata)
+        assert program_cm in associated_metadata
+
+        # Run again and expect that we will unassociate some content metadata
+        # from catalog query while perserving the content metadata objects
+        # themselves.
+        update_contentmetadata_from_discovery(catalog.uuid)
+        self.assertEqual(ContentMetadata.objects.count(), 3)
+
+        associated_metadata = catalog.catalog_query.contentmetadata_set.all()
+        assert course_cm not in associated_metadata
+        assert course_run_cm not in associated_metadata
+        assert program_cm in associated_metadata
