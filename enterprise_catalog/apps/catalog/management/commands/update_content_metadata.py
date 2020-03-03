32c0bbbe18c109b3f57f5d5cf2e52c42949a65f7
Brandon Baker <bbaker6225@users.noreply.github.com>
2020-03-03 13:13:35 -0500

    Add mgmt command to update all catalog metadata (#73)
    * Add mgmt command to update all catalog metadata
    
    This command is to be used as part of a jenkins job. The job is meant
    to be run daily in order to keep all of the content metadata associated
    with all catalogs up to date, and to update the associations between
    catalog queries and content metadata.
    
    ENT-2610

---
+++ enterprise_catalog/apps/catalog/management/commands/update_content_metadata.py
@@ -0,0 +1,25 @@
+import logging
+
+from django.core.management.base import BaseCommand
+
+from enterprise_catalog.apps.api.tasks import update_catalog_metadata_task
+from enterprise_catalog.apps.catalog.models import EnterpriseCatalog
+
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    help = (
+        'Update Content Metadata, along with the associations of Catalog Queries and Content Metadata, for all'
+        ' Enterprise Catalogs'
+    )
+
+    def handle(self, *args, **options):
+        for catalog in EnterpriseCatalog.objects.all():
+            update_catalog_metadata_task.delay(catalog_uuid=str(catalog.uuid))
+            message = (
+                'Spinning off update_catalog_metadata_task from update_content_metadata command'
+                ' to update content_metadata for catalog %s'
+            )
+            logger.info(message, catalog)
