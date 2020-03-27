4b7a643fbc969cf3603def157b2a218d991ff0fb
Brandon Baker <bbaker@edx.org>
2020-03-27 09:03:37 -0400

    Rename catalog endpoints to enterprise-catalogs
    Renaming these endpoints to match the old edx-enterprise resource
    naming.

--- enterprise_catalog/apps/api/v1/urls.py
+++ enterprise_catalog/apps/api/v1/urls.py
@@ -11,13 +11,13 @@ from enterprise_catalog.apps.api.v1 import views
 app_name = 'v1'
 
 router = DefaultRouter()  # pylint: disable=invalid-name
-router.register(r'enterprise-catalog', views.EnterpriseCatalogCRUDViewSet, basename='enterprise-catalog')
-router.register(r'enterprise-catalog', views.EnterpriseCatalogActionViewSet, basename='enterprise-catalog')
+router.register(r'enterprise-catalogs', views.EnterpriseCatalogCRUDViewSet, basename='enterprise-catalog')
+router.register(r'enterprise-catalogs', views.EnterpriseCatalogActionViewSet, basename='enterprise-catalog')
 router.register(r'enterprise-customer', views.EnterpriseCustomerViewSet, basename='enterprise-customer')
 
 urlpatterns = [
     url(
-        r'^enterprise-catalog/(?P<uuid>[\S]+)/refresh_metadata',
+        r'^enterprise-catalogs/(?P<uuid>[\S]+)/refresh_metadata',
         views.EnterpriseCatalogRefreshDataFromDiscovery.as_view({'post': 'post'}),
         name='update-enterprise-catalog'
     ),
