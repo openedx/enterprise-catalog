2d5557e27ed9851f63d15ded6a80657eed0de8d3
Adam Stankiewicz <astankiewicz@edx.org>
2020-05-26 18:36:36 -0400

    increase page_size query parameter on /search/all request (#117)

--- enterprise_catalog/apps/catalog/models.py
+++ enterprise_catalog/apps/catalog/models.py
@@ -403,9 +403,15 @@ def update_contentmetadata_from_discovery(catalog_uuid):
 
     catalog = EnterpriseCatalog.objects.get(uuid=catalog_uuid)
     catalog_query = catalog.catalog_query
-    query_params = {}
-    # Omit non-active course runs from the course-discovery results
-    query_params['exclude_expired_course_run'] = True
+    query_params = {
+        # Omit non-active course runs from the course-discovery results
+        'exclude_expired_course_run': True,
+        # Increasing page_size will help alleviate the non-deterministic behavior
+        # of the /search/all/ endpoint where a content item may appear on multiple
+        # pages (e.g., a course is included on both pages 1 and 2). This issue with the
+        # /search/all/ endpoint is likely the reason we get duplicate results.
+        'page_size': 200,
+    }
     metadata = client.get_metadata_by_query(catalog_query.content_filter, query_params=query_params)
     metadata_content_keys = [get_content_key(entry) for entry in metadata]
 
