54ba57c64fc8c5ad4d6e0b4e6d8c31280695c9d3
Adam Stankiewicz <agstanki@gmail.com>
2020-05-29 18:04:27 -0400

    run task only for queries used by at least one catalog

--- enterprise_catalog/apps/catalog/management/commands/tests/test_update_content_metadata.py
+++ enterprise_catalog/apps/catalog/management/commands/tests/test_update_content_metadata.py
@@ -3,7 +3,10 @@ from django.core.management import call_command
 from django.test import TestCase
 
 from enterprise_catalog.apps.api.tasks import update_catalog_metadata_task
-from enterprise_catalog.apps.catalog.tests.factories import CatalogQueryFactory
+from enterprise_catalog.apps.catalog.tests.factories import (
+    CatalogQueryFactory,
+    EnterpriseCatalogFactory,
+)
 
 
 class UpdateContentMetadataCommandTests(TestCase):
@@ -12,13 +15,40 @@ class UpdateContentMetadataCommandTests(TestCase):
     @mock.patch(
         'enterprise_catalog.apps.catalog.management.commands.update_content_metadata.update_catalog_metadata_task.delay'
     )
-    def test_update_content_metadata(self, mock_task):
+    def test_update_content_metadata_for_all_queries(self, mock_task):
         """
-        Verify that the job creates an update task for every enterprise catalog
+        Verify that the job creates an update task for every catalog query
         """
         [catalog_query_a, catalog_query_b, catalog_query_c] = CatalogQueryFactory.create_batch(3)
+
+        enterprise_catalog_a = EnterpriseCatalogFactory(catalog_query=catalog_query_a)
+        enterprise_catalog_b = EnterpriseCatalogFactory(catalog_query=catalog_query_b)
+        enterprise_catalog_c = EnterpriseCatalogFactory(catalog_query=catalog_query_c)
+
         call_command(self.command_name)
+
+        assert mock_task.call_count == 3
 
         mock_task.assert_any_call(catalog_query_id=catalog_query_a.id)
         mock_task.assert_any_call(catalog_query_id=catalog_query_b.id)
         mock_task.assert_any_call(catalog_query_id=catalog_query_c.id)
+
+    @mock.patch(
+        'enterprise_catalog.apps.catalog.management.commands.update_content_metadata.update_catalog_metadata_task.delay'
+    )
+    def test_update_content_metadata_for_filtered_queries(self, mock_task):
+        """
+        Verify that the job creates an update task for every catalog query that is used by
+        at least one enterprise catalog.
+        """
+        [catalog_query_a, catalog_query_b, catalog_query_c] = CatalogQueryFactory.create_batch(3)
+
+        enterprise_catalog_a = EnterpriseCatalogFactory(catalog_query=catalog_query_a)
+        enterprise_catalog_b = EnterpriseCatalogFactory(catalog_query=catalog_query_b)
+
+        call_command(self.command_name)
+
+        assert mock_task.call_count == 2
+
+        mock_task.assert_any_call(catalog_query_id=catalog_query_a.id)
+        mock_task.assert_any_call(catalog_query_id=catalog_query_b.id)
