98938e8d56f54c33900c987bd87fd492b7191957
Adam Stankiewicz <astankiewicz@edx.org>
2020-05-30 11:33:21 -0400

    run task only for queries used by at least one catalog (#124)
    * run task only for queries used by at least one catalog
    
    * quality

--- enterprise_catalog/apps/catalog/management/commands/update_content_metadata.py
+++ enterprise_catalog/apps/catalog/management/commands/update_content_metadata.py
@@ -3,7 +3,10 @@ import logging
 from django.core.management.base import BaseCommand
 
 from enterprise_catalog.apps.api.tasks import update_catalog_metadata_task
-from enterprise_catalog.apps.catalog.models import CatalogQuery
+from enterprise_catalog.apps.catalog.models import (
+    CatalogQuery,
+    EnterpriseCatalog,
+)
 
 
 logger = logging.getLogger(__name__)
@@ -11,12 +14,13 @@ logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     help = (
-        'Update Content Metadata, along with the associations of Catalog Queries and Content Metadata, for all'
-        ' Enterprise Catalogs'
+        'Update Content Metadata, along with the associations of Catalog Queries and Content Metadata',
     )
 
     def handle(self, *args, **options):
-        for catalog_query in CatalogQuery.objects.all():
+        # Iterate through all CatalogQuery records used by at least one EnterpriseCatalog to avoid
+        # calling /search/all/ for a CatalogQuery that is not currently used by any catalogs.
+        for catalog_query in CatalogQuery.objects.filter(enterprise_catalogs__isnull=False).distinct():
             update_catalog_metadata_task.delay(catalog_query_id=catalog_query.id)
             message = (
                 'Spinning off update_catalog_metadata_task from update_content_metadata command'
