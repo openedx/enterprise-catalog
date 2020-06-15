0ca5f71dff35d0fb73bccf359155db5a1ee9dfe0
Talia Rhodes <taliarhodes@C02C119QLVDM.tld>
2020-06-15 13:32:15 -0400

    ENH: Add content filter validation for 3 subfields.

--- enterprise_catalog/apps/catalog/forms.py
+++ enterprise_catalog/apps/catalog/forms.py
@@ -6,6 +6,7 @@ from django import forms
 from django.core.exceptions import ValidationError
 from edx_rbac.admin import UserRoleAssignmentAdminForm
 
+from enterprise_catalog.apps.catalog.constants import CONTENT_FILTER_FIELD_TYPES
 from enterprise_catalog.apps.catalog.models import (
     CatalogQuery,
     EnterpriseCatalogRoleAssignment,
@@ -22,8 +23,25 @@ class CatalogQueryForm(forms.ModelForm):
         model = CatalogQuery
         fields = ('content_filter',)
 
+    def validate_content_filter_fields(self, content_filter):
+        for key in CONTENT_FILTER_FIELD_TYPES:
+            if key in content_filter.keys():
+                if not isinstance(content_filter[key], CONTENT_FILTER_FIELD_TYPES[key]['type']):
+                    raise ValidationError(
+                        "Content filter '%s' must be of type %s" % (key, CONTENT_FILTER_FIELD_TYPES[key]['type'])
+                    )
+                if CONTENT_FILTER_FIELD_TYPES[key]['type'] == list:
+                    if not all(isinstance(x, str) for x in content_filter[key]):
+                        raise ValidationError(
+                            "Content filter '%s' must contain values of type %s" % (
+                                key, CONTENT_FILTER_FIELD_TYPES[key]['subtype']
+                            )
+                        )
+
     def clean_content_filter(self):
         content_filter = self.cleaned_data['content_filter']
+        self.validate_content_filter_fields(content_filter)
+
         content_filter_hash = get_content_filter_hash(content_filter)
         if CatalogQuery.objects.filter(content_filter_hash=content_filter_hash).exists():
             raise ValidationError('Catalog Query with this Content filter already exists.')
---
+++ enterprise_catalog/apps/catalog/tests/test_forms.py
@@ -0,0 +1,36 @@
+import ddt
+from django.test import TestCase
+
+from enterprise_catalog.apps.catalog.forms import CatalogQueryForm
+
+
+@ddt.ddt
+class TestCatalogQueryAdmin(TestCase):
+    @ddt.data(
+        ({'content_filter': {'key': 'coursev1:course1'}}, ["Content filter 'key' must be of type <class 'list'>"]),
+        (
+            {'content_filter': {'aggregation_key': 'courserun:course'}},
+            ["Content filter 'aggregation_key' must be of type <class 'list'>"],
+        ),
+        (
+            {'content_filter': {'first_enrollable_paid_seat_price__lte': [12]}},
+            ["Content filter 'first_enrollable_paid_seat_price__lte' must be of type <class 'str'>"]
+        ),
+        ({'content_filter': {'key': [3, 'course']}}, ["Content filter 'key' must contain values of type <class 'str'>"])
+    )
+    @ddt.unpack
+    def test_catalog_query_form_failure(self, cfdata, error):
+        form = CatalogQueryForm(data=cfdata)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['content_filter'], error)
+
+    @ddt.data(
+        ({'content_filter': {'key': ['coursev1:course1', 'coursev1:course2']}},),
+        ({'content_filter': {'aggregation_key': ['courserun:course', 'courserun:course2']}},),
+        ({'content_filter': {'first_enrollable_paid_seat_price__lte': '12'}},),
+        ({'content_filter': {'key': ['coursev1:course1'], 'first_enrollable_paid_seat_price__lte': '50'}},)
+    )
+    @ddt.unpack
+    def test_catalog_query_form_success(self, cfdata):
+        form = CatalogQueryForm(data=cfdata)
+        self.assertTrue(form.is_valid())
