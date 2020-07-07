883547bba09380785ea74db6967871ae77618ac9
Adam Stankiewicz <agstanki@gmail.com>
2020-07-07 13:48:43 -0400

    remove semicolon

--- enterprise_catalog/apps/api/v1/utils.py
+++ enterprise_catalog/apps/api/v1/utils.py
@@ -204,7 +204,7 @@ def get_course_subjects(course):
     subjects = course.get('subjects', [])
 
     if len(subjects) > 0 and isinstance(subjects[0], str):
-        logger.info('Subjects for course %s: %s', course.get('key'), subjects);
+        logger.info('Subjects for course %s: %s', course.get('key'), subjects)
 
     for subject in subjects:
         subject_name = subject.get('name')
