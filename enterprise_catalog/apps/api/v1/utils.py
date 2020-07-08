bc9142bb775e37dd4d179cac6fa605e33c5ff82a
Adam Stankiewicz <agstanki@gmail.com>
2020-07-08 06:19:28 -0400

    remove temp logging

--- enterprise_catalog/apps/api/v1/utils.py
+++ enterprise_catalog/apps/api/v1/utils.py
@@ -201,10 +201,7 @@ def get_course_subjects(course):
         list: a list of subject names associated with the course
     """
     subject_names = set()
-    subjects = course.get('subjects', [])
-
-    if len(subjects) > 0 and isinstance(subjects[0], str):
-        logger.info('Subjects for course %s: %s', course.get('key'), subjects)
+    subjects = course.get('subjects') or []
 
     for subject in subjects:
         subject_name = subject.get('name')
