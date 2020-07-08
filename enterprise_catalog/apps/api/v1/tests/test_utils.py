a54890226bb954c00c30a220bfb30d3089058895
Adam Stankiewicz <agstanki@gmail.com>
2020-07-08 07:16:26 -0400

    quality fix

--- enterprise_catalog/apps/api/v1/tests/test_utils.py
+++ enterprise_catalog/apps/api/v1/tests/test_utils.py
@@ -35,9 +35,9 @@ class EnterpriseCatalogApiUtilsTests(TestCase):
     )
     @ddt.unpack
     def test_get_course_subjects(self, course_metadata, expected_subjects):
-            """
-            Assert get_course_subjects is flexible enough to support both a list of strings
-            and a list of dictionaries.
-            """
-            course_subjects = get_course_subjects(course_metadata)
-            assert sorted(course_subjects) == sorted(expected_subjects)
+        """
+        Assert get_course_subjects is flexible enough to support both a list of strings
+        and a list of dictionaries.
+        """
+        course_subjects = get_course_subjects(course_metadata)
+        assert sorted(course_subjects) == sorted(expected_subjects)
