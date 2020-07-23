bfc607ba05f3a1b0ab5ec692e768265f869ca239
Adam Blackwell <ablackwell@edx.org>
2020-07-23 18:23:16 -0400

    Remove django_site sql.

--- provision-mysql.sql
+++ provision-mysql.sql
@@ -9,15 +9,4 @@ CREATE DATABASE IF NOT EXISTS edxapp_csmh;
 GRANT ALL ON edxapp.* TO 'edxapp001'@'%' IDENTIFIED BY 'password';
 GRANT ALL ON edxapp_csmh.* TO 'edxapp001'@'%';
 
-# TODO: Remove this and find a better home.
---
--- Dumping data for table `django_site`
---
-
-LOCK TABLES `django_site` WRITE;
-/*!40000 ALTER TABLE `django_site` DISABLE KEYS */;
-INSERT INTO `django_site` VALUES (1,'example.com','example.com');
-/*!40000 ALTER TABLE `django_site` ENABLE KEYS */;
-UNLOCK TABLES;
-
 FLUSH PRIVILEGES;
