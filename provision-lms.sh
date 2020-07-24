f18ebb2787de8ccebee326239453e2df32ee01d7
Kyle McCormick <kmccormick@edx.org>
2020-07-24 14:34:48 -0400

    Reorder DB provisioning

--- provision-lms.sh
+++ provision-lms.sh
@@ -3,25 +3,6 @@
 # Include utilities.
 source provisioning-utils.sh
 
-log_step "lms: Making sure MongoDB is ready..."
-until docker-compose exec -T mongo bash -c 'mongo --eval "printjson(db.serverStatus())"' &> /dev/null
-do
-  printf "."
-  sleep 1
-done
-
-log_step "MongoDB ready. Adding default MongoDB data..."
-service_exec mongo mongorestore --gzip /data/dump
-
-log_step "MongoDB ready. Creating MongoDB users..."
-docker-compose exec -T mongo bash -c "mongo" < provision-mongo.js
-
-log_step "Adding default MySQL data from dump..."
-cat provision-mysql_from-devstack.sql | docker-compose exec -T mysql /usr/bin/mysql edxapp
-
-log_step "Ensuring MySQL databases and users exist again..."
-docker-compose exec -T mysql bash -c "mysql -uroot mysql" < provision-mysql.sql
-
 # TODO: Make sure this handles squashed migrations idempotently 
 # (e.g. enterprise/migrations/0001_squashed_0092_auto_20200312_1650.py)
 #log_step "lms: Running migrations for default database..."
--- provision.sh
+++ provision.sh
@@ -13,8 +13,8 @@ log_step "Starting provisioning process..."
 # log_step "Pulling latest images..."
 # docker-compose pull --include-deps app
 
-log_step "Bringing up containers..."
-docker-compose up --detach app
+log_step "Bringing up database containers..."
+docker-compose up --detach mysql mongo elasticsearch
 
 is_mysql_ready(){
 	docker-compose exec \
@@ -42,8 +42,27 @@ if ! is_mysql_ready; then
 	sleep 5
 fi
 
+log_step "Adding default MySQL data from dump..."
+docker-compose exec -T mysql /usr/bin/mysql edxapp < provision-mysql_from-devstack.sql
+
 log_step "Ensuring MySQL databases and users exist..."
 docker-compose exec -T mysql bash -c "mysql -uroot mysql" < provision-mysql.sql
+
+log_step "lms: Making sure MongoDB is ready..."
+until docker-compose exec -T mongo bash -c 'mongo --eval "printjson(db.serverStatus())"' &> /dev/null
+do
+  printf "."
+  sleep 1
+done
+
+log_step "MongoDB ready. Creating MongoDB users..."
+docker-compose exec -T mongo bash -c "mongo" < provision-mongo.js
+
+log_step "MongoDB ready. Adding default MongoDB data..."
+service_exec mongo mongorestore --gzip /data/dump
+
+log_step "Bringing up app containers..."
+docker-compose up --detach app
 
 # Run provisioning scripts for dependencies.
 # We call provision-lms.sh, provision-discovery.sh, etc., and log an error
