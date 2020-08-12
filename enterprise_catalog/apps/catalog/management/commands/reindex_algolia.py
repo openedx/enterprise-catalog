688546f56fcf98ddf4dc3a9c480c2ba53997c634
Brandon Baker <bbaker@edx.org>
2020-08-12 10:16:44 -0400

    Match B2C course indexing for algolia
    Replicates the logic from course-discovery that restricts which courses
    are indexed for algolia search.
    Some pieces of the logic look a little bit different as we use fields
    coming from the API whereas they use fields in the discovery database.
    Additionally, there is some logic in discovery that the website team
    felt was duplicated that has been omitted here.
    
    ENT-2992

--- enterprise_catalog/apps/catalog/management/commands/reindex_algolia.py
+++ enterprise_catalog/apps/catalog/management/commands/reindex_algolia.py
@@ -67,6 +67,40 @@ ALGOLIA_INDEX_SETTINGS = {
 }
 
 
+def should_index(course_metadata):
+    """
+    Replicates the B2C index check of whether a certain course should be indexed for search.
+
+    A course should only be indexed for algolia search if it has a non-indexed advertiseable course run, at least
+    one owner, and a marketing url slug.
+    The course-discovery check that the course's partner is edX is included by default as the discovery API filters
+    to the request's site's partner.
+    The discovery check that the course has an availability level was decided to be a duplicate check that the
+    website team plans on removing.
+    Original code:
+    https://github.com/edx/course-discovery/blob/c6ac5329225e2f32cdf1d1da855d7c9d905b2576/course_discovery/apps/course_metadata/algolia_models.py#L218-L227
+
+    Args:
+        course (ContentMetadata): The ContentMetadata representing a course object.
+
+    Returns:
+        bool: Whether or not the course should be indexed by algolia.
+    """
+    course_json_metadata = course_metadata.json_metadata
+    advertised_course_run_uuid = course_json_metadata.get('advertised_course_run_uuid')
+    course_runs = course_json_metadata.get('course_runs')
+    try:
+        advertised_course_run = [run for run in course_runs if run.get('uuid') == advertised_course_run_uuid][0]
+    except IndexError:
+        # If there is no advertised course run we can immediately return False
+        return False
+
+    owners = course_json_metadata.get('owners', [])
+    return (len(owners) > 0
+            and bool(course_json_metadata.get('url_slug'))
+            and not advertised_course_run.get('hidden'))
+
+
 class Command(BaseCommand):
     help = (
         'Reindex course data in Algolia, adding on enterprise-specific metadata'
@@ -85,16 +119,13 @@ class Command(BaseCommand):
         algolia_client.set_index_settings(ALGOLIA_INDEX_SETTINGS)
 
         # retrieve content_keys for all ContentMetadata records with a content type of "course"
-        course_content_metadata = content_metadata_with_type_course()
-        content_metadata_keys = []
-        if course_content_metadata:
-            content_metadata_keys = [
-                metadata['content_key']
-                for metadata in course_content_metadata.values('content_key')
-            ]
+        all_course_content_metadata = content_metadata_with_type_course()
+        # Only use the course content metadata that should be indexed for Algolia using the B2C logic
+        indexable_content_keys = [course_content_metadata.content_key for course_content_metadata
+                                  in all_course_content_metadata if should_index(course_content_metadata)]
 
         # batch the content keys and spin off a new task for each batch
-        for content_keys_batch in batch(content_metadata_keys, batch_size=BATCH_SIZE):
+        for content_keys_batch in batch(indexable_content_keys, batch_size=BATCH_SIZE):
             async_task = index_enterprise_catalog_courses_in_algolia_task.delay(
                 algolia_fields=ALGOLIA_FIELDS,
                 content_keys=content_keys_batch,
