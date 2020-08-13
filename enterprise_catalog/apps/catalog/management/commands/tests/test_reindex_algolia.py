e595b44971678bbd87b16167ff2dbd7a697ca726
Brandon Baker <bbaker6225@users.noreply.github.com>
2020-08-13 14:17:58 -0400

    Match B2C course indexing for algolia (#157)
    * Match B2C course indexing for algolia
    
    Replicates the logic from course-discovery that restricts which courses
    are indexed for algolia search.
    Some pieces of the logic look a little bit different as we use fields
    coming from the API whereas they use fields in the discovery database.
    Additionally, there is some logic in discovery that the website team
    felt was duplicated that has been omitted here.
    
    ENT-2992

--- enterprise_catalog/apps/catalog/management/commands/tests/test_reindex_algolia.py
+++ enterprise_catalog/apps/catalog/management/commands/tests/test_reindex_algolia.py
@@ -1,3 +1,6 @@
+from uuid import uuid4
+
+import ddt
 import mock
 from django.core.management import call_command
 from django.test import TestCase
@@ -7,25 +10,73 @@ from enterprise_catalog.apps.catalog.management.commands.reindex_algolia import 
     ALGOLIA_FIELDS,
     ALGOLIA_INDEX_SETTINGS,
     BATCH_SIZE,
+    should_index_course,
 )
 from enterprise_catalog.apps.catalog.tests.factories import (
     ContentMetadataFactory,
 )
 
 
+@ddt.ddt
 class ReindexAlgoliaCommandTests(TestCase):
     command_name = 'reindex_algolia'
 
+    @ddt.data(
+        {'expected_result': False, 'has_advertised_course_run': False},
+        {'expected_result': False, 'has_owners': False},
+        {'expected_result': False, 'has_url_slug': False},
+        {'expected_result': False, 'advertised_course_run_hidden': True},
+        {'expected_result': True},
+    )
+    @ddt.unpack
+    def test_should_index_course(
+        self,
+        expected_result,
+        has_advertised_course_run=True,
+        has_owners=True,
+        has_url_slug=True,
+        advertised_course_run_hidden=False,
+    ):
+        """
+        Verify that only a course that has a non-hidden advertised course run, at least one owner, and a marketing slug
+        is marked as indexable.
+        """
+        advertised_course_run_uuid = uuid4()
+        course_run_uuid = advertised_course_run_uuid if has_advertised_course_run else uuid4()
+        owners = [{'name': 'edX'}] if has_owners else []
+        url_slug = 'test-slug' if has_url_slug else ''
+        json_metadata = {
+            'advertised_course_run_uuid': advertised_course_run_uuid,
+            'course_runs': [
+                {
+                    'hidden': advertised_course_run_hidden,
+                    'uuid': course_run_uuid,
+                },
+            ],
+            'owners': owners,
+            'url_slug': url_slug,
+        }
+        course_metadata = ContentMetadataFactory.create(
+            content_type=COURSE,
+            json_metadata=json_metadata,
+        )
+        assert should_index_course(course_metadata) is expected_result
+
+    @mock.patch(
+        'enterprise_catalog.apps.catalog.management.commands.reindex_algolia.should_index_course'
+    )
     @mock.patch(
         'enterprise_catalog.apps.catalog.management.commands.reindex_algolia.AlgoliaSearchClient'
     )
     @mock.patch(
         'enterprise_catalog.apps.catalog.management.commands.reindex_algolia.index_enterprise_catalog_courses_in_algolia_task'
     )
-    def test_reindex_algolia(self, mock_task, mock_search_client):
+    def test_reindex_algolia(self, mock_task, mock_search_client, mock_should_index_course):
         """
         Verify that the job spins off the correct number of index_enterprise_catalog_courses_in_algolia_task
         """
+        # Mock that all courses should be indexed
+        mock_should_index_course.return_value = True
         # create enough ContentMetadata records to force more than 1 batch of content keys
         content_metadata = ContentMetadataFactory.create_batch(
             BATCH_SIZE + 1,
