0eddf49df602f327000564173fb033a700080721
Manjinder Singh <49171515+jinder1s@users.noreply.github.com>
2020-09-02 14:32:37 -0400

    Update decentralized_devstack/provision-lms.sh
    Co-authored-by: Tim McCormack <tmccormack@edx.org>

--- decentralized_devstack/provision-lms.sh
+++ decentralized_devstack/provision-lms.sh
@@ -3,7 +3,7 @@
 # Include utilities.
 source decentralized_devstack/provisioning-utils.sh
 
-# only load mysql schema if database does not exist, this way reprovisioning will not overright data
+# only load mysql schema if database does not exist, this way reprovisioning will not overwrite data
 edxapp_count=$(docker-compose exec mysql mysql -uroot --skip-column-names -se "SELECT COUNT(*) FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME='edxapp';" 2>/dev/null | tr -d $'\r')
 if [[ "${edxapp_count}" = "1" ]];
 then
