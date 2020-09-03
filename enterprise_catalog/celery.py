0048f362cacf43b56a6ebd711f133544b240353d
Awais Qureshi <awais.qureshi@arbisoft.com>
2020-09-03 17:04:25 +0500

    Revert "upgrade celery to 4.2.2"

--- enterprise_catalog/celery.py
+++ enterprise_catalog/celery.py
@@ -9,8 +9,7 @@ app = Celery('enterprise_catalog', )
 
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
-app.conf.task_protocol = 1
-app.config_from_object('django.conf:settings')
+app.config_from_object(settings)
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
