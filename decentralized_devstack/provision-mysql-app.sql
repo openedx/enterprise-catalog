8a0fa7f1932c5fc65b82090afb5f4bfbc45473be
Manjinder Singh <49171515+jinder1s@users.noreply.github.com>
2020-09-25 13:36:00 -0400

    give entcatalog001* user access to test_enterprise_catalog database (#200)

--- decentralized_devstack/provision-mysql-app.sql
+++ decentralized_devstack/provision-mysql-app.sql
@@ -3,4 +3,6 @@ FLUSH PRIVILEGES;
 CREATE DATABASE IF NOT EXISTS enterprise_catalog;
 GRANT ALL ON enterprise_catalog.* TO 'entcatalog001'@'%' IDENTIFIED BY 'password';
 
+GRANT ALL ON test_enterprise_catalog.* TO 'entcatalog001'@'%' IDENTIFIED BY 'password';
+
 FLUSH PRIVILEGES;
