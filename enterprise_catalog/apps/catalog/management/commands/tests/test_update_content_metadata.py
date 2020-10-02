13358392a2f9a6405b36b0bc60bb7af4f97c662e
Brandon Baker <bbaker@edx.org>
2020-10-02 11:39:54 -0400

    Allow updating a specified subset of catalogs
    Adds an optional kwarg `catalog_uuids` to the update_content_metadata
    command which allows users to only update the ContentMetadata for the
    catalogs that are associated with the catalog query of the provided
    catalog.
    Note that this does not change the update_full_content_metadata task to
    only do that process for the subset of content associated with the
    catalog queries of the enterprise catalogs that are given here. That is
    being split out to try and make the refactoring cleaner.

--- enterprise_catalog/apps/catalog/management/commands/tests/test_update_content_metadata.py
+++ enterprise_catalog/apps/catalog/management/commands/tests/test_update_content_metadata.py
@@ -16,6 +16,14 @@ from enterprise_catalog.apps.catalog.tests.factories import (
 class UpdateContentMetadataCommandTests(TestCase):
     command_name = 'update_content_metadata'
 
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.catalog_query_a = CatalogQueryFactory()
+        cls.catalog_query_b = CatalogQueryFactory()
+        cls.enterprise_catalog_a = EnterpriseCatalogFactory(catalog_query=cls.catalog_query_a)
+        cls.enterprise_catalog_b = EnterpriseCatalogFactory(catalog_query=cls.catalog_query_b)
+
     def tearDown(self):
         super(UpdateContentMetadataCommandTests, self).tearDown()
         # clean up any stale test objects
@@ -29,18 +37,11 @@ class UpdateContentMetadataCommandTests(TestCase):
         """
         Verify that the job creates an update task for every catalog query
         """
-        [catalog_query_a, catalog_query_b, catalog_query_c] = CatalogQueryFactory.create_batch(3)
-
-        enterprise_catalog_a = EnterpriseCatalogFactory(catalog_query=catalog_query_a)
-        enterprise_catalog_b = EnterpriseCatalogFactory(catalog_query=catalog_query_b)
-        enterprise_catalog_c = EnterpriseCatalogFactory(catalog_query=catalog_query_c)
-
         call_command(self.command_name)
 
         mock_chord.assert_called_once_with([
-            mock_catalog_task.s(catalog_query_id=catalog_query_a),
-            mock_catalog_task.s(catalog_query_id=catalog_query_b),
-            mock_catalog_task.s(catalog_query_id=catalog_query_c),
+            mock_catalog_task.s(catalog_query_id=self.catalog_query_a),
+            mock_catalog_task.s(catalog_query_id=self.catalog_query_b),
         ])
         mock_full_metadata_task.s.assert_called_once()
 
@@ -52,15 +53,27 @@ class UpdateContentMetadataCommandTests(TestCase):
         Verify that the job creates an update task for every catalog query that is used by
         at least one enterprise catalog.
         """
-        [catalog_query_a, catalog_query_b, catalog_query_c] = CatalogQueryFactory.create_batch(3)
-
-        enterprise_catalog_a = EnterpriseCatalogFactory(catalog_query=catalog_query_a)
-        enterprise_catalog_b = EnterpriseCatalogFactory(catalog_query=catalog_query_b)
+        # Create another catalog query that isn't used by any catalog, so shouldn't be updated
+        CatalogQueryFactory()
 
         call_command(self.command_name)
 
         mock_chord.assert_called_once_with([
-            mock_catalog_task.s(catalog_query_id=catalog_query_a),
-            mock_catalog_task.s(catalog_query_id=catalog_query_b),
+            mock_catalog_task.s(catalog_query_id=self.catalog_query_a),
+            mock_catalog_task.s(catalog_query_id=self.catalog_query_b),
+        ])
+        mock_full_metadata_task.s.assert_called_once()
+
+    @mock.patch('enterprise_catalog.apps.catalog.management.commands.update_content_metadata.chord')
+    @mock.patch('enterprise_catalog.apps.catalog.management.commands.update_content_metadata.update_catalog_metadata_task')
+    @mock.patch('enterprise_catalog.apps.catalog.management.commands.update_content_metadata.update_full_content_metadata_task')
+    def test_update_content_metadata_with_args(self, mock_full_metadata_task, mock_catalog_task, mock_chord):
+        """
+        Verify that the job only updates the catalog query associated with the provided catalog_uuid if given.
+        """
+        call_command(self.command_name, catalog_uuids=[self.enterprise_catalog_a.uuid])
+
+        mock_chord.assert_called_once_with([
+            mock_catalog_task.s(catalog_query_id=self.catalog_query_a),
         ])
         mock_full_metadata_task.s.assert_called_once()
