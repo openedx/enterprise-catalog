1a860d573931df1ead4f0d7659268cfe2d061a12
Brandon Baker <bbaker@edx.org>
2020-10-09 13:26:00 -0400

    Update manual & auto catalog refresh functionality
    * Updates the refresh_metadata endpoint for a catalog to perform all the
    steps of `update_content_metadata`, `update_full_content_metadata`, and
    `reindex_algolia` one after the other for the content associated with
    the given catalog's CatalogQuery. This is done both to bring the
    endpoint back in sync with the functionality of the aforementioned
    scheduled jenkins jobs, as well as to give ECS and developers an easy
    way to update a single CatalogQuery at a time (used in onboarding new
    EnterpriseCustomers / EnterpriseCatalogs).
    * Significant refactoring of the current tasks used by the jobs to
    support both flows
    * Minor quality and test fixes
    * Have the update_content_metadata mgmt command block until the async result is ready,
    or 30 minutes have passed, whichever comes first.
    
    ENT-2450

--- enterprise_catalog/apps/api/tests/test_tasks.py
+++ enterprise_catalog/apps/api/tests/test_tasks.py
@@ -15,40 +15,77 @@ from enterprise_catalog.apps.catalog.tests.factories import (
 )
 
 
-class EnterpriseCatalogCeleryTaskTests(TestCase):
+class UpdateCatalogMetadataTaskTests(TestCase):
+    """
+    Tests for the `update_catalog_metadata_task`.
+    """
+
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.catalog_query = CatalogQueryFactory()
+
     @mock.patch('enterprise_catalog.apps.api.tasks.update_contentmetadata_from_discovery')
-    def test_refresh_metadata(self, update_contentmetadata_from_discovery_mock):
+    def test_update_catalog_metadata(self, mock_update_data_from_discovery):
         """
         Assert update_catalog_metadata_task is called with correct catalog_query_id
         """
-        catalog_query = CatalogQueryFactory()
-        tasks.update_catalog_metadata_task(catalog_query.id)
-        update_contentmetadata_from_discovery_mock.assert_called_with(catalog_query.id)
+        tasks.update_catalog_metadata_task(self.catalog_query.id)
+        mock_update_data_from_discovery.assert_called_with(self.catalog_query)
 
+    @mock.patch('enterprise_catalog.apps.api.tasks.update_contentmetadata_from_discovery')
+    def test_update_catalog_metadata_no_catalog_query(self, mock_update_data_from_discovery):
+        """
+        Assert that discovery is not called if a bad catalog query id is passed
+        """
+        bad_id = 412
+        tasks.update_catalog_metadata_task(bad_id)
+        mock_update_data_from_discovery.assert_not_called()
+
+
+class UpdateFullContentMetadataTaskTests(TestCase):
+    """
+    Tests for the `update_full_content_metadata_task`.
+    """
+
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.enterprise_catalog = EnterpriseCatalogFactory()
+        cls.catalog_query = cls.enterprise_catalog.catalog_query
+
+    @mock.patch('enterprise_catalog.apps.api.tasks.get_indexable_course_keys')
     @mock.patch('enterprise_catalog.apps.api_client.base_oauth.OAuthAPIClient')
-    def test_update_full_metadata(self, mock_oauth_client):
+    def test_update_full_metadata(self, mock_oauth_client, mock_get_indexable_course_keys):
         """
         Assert that full course metadata is merged with original json_metadata for all ContentMetadata records.
         """
-        course_data_1 = {'key': 'fakeX', 'full_course_only_field': 'test_1'}
-        course_data_2 = {'key': 'testX', 'full_course_only_field': 'test_2'}
+        course_key_1 = 'fakeX'
+        course_data_1 = {'key': course_key_1, 'full_course_only_field': 'test_1'}
+        course_key_2 = 'testX'
+        course_data_2 = {'key': course_key_2, 'full_course_only_field': 'test_2'}
+        non_course_key = 'course-runX'
 
+        # Mock out the data that should be returned from discovery's /api/v1/courses endpoint
         mock_oauth_client.return_value.get.return_value.json.return_value = {
             'results': [course_data_1, course_data_2],
         }
 
-        enterprise_catalog = EnterpriseCatalogFactory()
-        catalog_query = enterprise_catalog.catalog_query
-
-        metadata_1 = ContentMetadataFactory(content_type=COURSE, content_key='fakeX')
-        metadata_1.catalog_queries.set([catalog_query])
-        metadata_2 = ContentMetadataFactory(content_type=COURSE, content_key='testX')
-        metadata_2.catalog_queries.set([catalog_query])
+        metadata_1 = ContentMetadataFactory(content_type=COURSE, content_key=course_key_1)
+        metadata_1.catalog_queries.set([self.catalog_query])
+        metadata_2 = ContentMetadataFactory(content_type=COURSE, content_key=course_key_2)
+        metadata_2.catalog_queries.set([self.catalog_query])
+        non_course_metadata = ContentMetadataFactory(content_type=COURSE_RUN, content_key=non_course_key)
+        non_course_metadata.catalog_queries.set([self.catalog_query])
 
         assert metadata_1.json_metadata != course_data_1
         assert metadata_2.json_metadata != course_data_2
 
-        tasks.update_full_content_metadata_task()
+        mock_get_indexable_course_keys.return_value = [course_key_1, course_key_2]
+        indexable_course_keys = tasks.update_full_content_metadata_task([course_key_1, course_key_2, non_course_key])
+        # Verify the task returns the course keys that are indexable as those keys are passed to the
+        # `index_enterprise_catalog_courses_in_algolia_task` from the `EnterpriseCatalogRefreshDataFromDiscovery` view.
+        assert indexable_course_keys == mock_get_indexable_course_keys.return_value
 
         metadata_1 = ContentMetadata.objects.get(content_key='fakeX')
         metadata_2 = ContentMetadata.objects.get(content_key='testX')
@@ -73,48 +110,57 @@ class EnterpriseCatalogCeleryTaskTests(TestCase):
         assert metadata_1.json_metadata == course_data_1
         assert metadata_2.json_metadata == course_data_2
 
-    def _set_up_factory_data_for_algolia(self):
-        # set up new catalog, query, and metadata for a course
-        enterprise_catalog_courses = EnterpriseCatalogFactory()
-        courses_catalog_query = enterprise_catalog_courses.catalog_query
-        course_metadata = ContentMetadataFactory(content_type=COURSE, content_key='fakeX')
-        course_metadata.catalog_queries.set([courses_catalog_query])
 
-        # set up new catalog, query, and metadata for a course run
-        enterprise_catalog_course_runs = EnterpriseCatalogFactory()
-        course_runs_catalog_query = enterprise_catalog_course_runs.catalog_query
+class IndexEnterpriseCatalogCoursesInAlgoliaTaskTests(TestCase):
+    """
+    Tests for `index_enterprise_catalog_courses_in_algolia_task`
+    """
+
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.ALGOLIA_FIELDS = ['key', 'objectID', 'enterprise_customer_uuids', 'enterprise_catalog_uuids']
+
+        # Set up a catalog, query, and metadata for a course
+        cls.enterprise_catalog_courses = EnterpriseCatalogFactory()
+        courses_catalog_query = cls.enterprise_catalog_courses.catalog_query
+        cls.course_metadata = ContentMetadataFactory(content_type=COURSE, content_key='fakeX')
+        cls.course_metadata.catalog_queries.set([courses_catalog_query])
+
+        # Set up new catalog, query, and metadata for a course run
+        cls.enterprise_catalog_course_runs = EnterpriseCatalogFactory()
+        course_runs_catalog_query = cls.enterprise_catalog_course_runs.catalog_query
         course_run_metadata = ContentMetadataFactory(content_type=COURSE_RUN, parent_content_key='fakeX')
         course_run_metadata.catalog_queries.set([course_runs_catalog_query])
 
+    def _set_up_factory_data_for_algolia(self):
         expected_catalog_uuids = sorted([
-            str(enterprise_catalog_courses.uuid),
-            str(enterprise_catalog_course_runs.uuid)
+            str(self.enterprise_catalog_courses.uuid),
+            str(self.enterprise_catalog_course_runs.uuid)
         ])
         expected_customer_uuids = sorted([
-            str(enterprise_catalog_courses.enterprise_uuid),
-            str(enterprise_catalog_course_runs.enterprise_uuid),
+            str(self.enterprise_catalog_courses.enterprise_uuid),
+            str(self.enterprise_catalog_course_runs.enterprise_uuid),
         ])
 
         return {
             'catalog_uuids': expected_catalog_uuids,
             'customer_uuids': expected_customer_uuids,
-            'course_metadata': course_metadata,
+            'course_metadata': self.course_metadata,
         }
 
-    @mock.patch('enterprise_catalog.apps.api.tasks.AlgoliaSearchClient')
+    @mock.patch('enterprise_catalog.apps.api.tasks.get_initialized_algolia_client', return_value=mock.MagicMock())
     def test_index_algolia_with_all_uuids(self, mock_search_client):
         """
         Assert that the correct data is sent to Algolia index, with the expected enterprise
         catalog and enterprise customer associations.
         """
-        ALGOLIA_FIELDS = ['key', 'objectID', 'enterprise_customer_uuids', 'enterprise_catalog_uuids']
-
         algolia_data = self._set_up_factory_data_for_algolia()
 
-        tasks.index_enterprise_catalog_courses_in_algolia_task(ALGOLIA_FIELDS, content_keys=['fakeX'])
-
-        # verify Algolia index is initialized
-        mock_search_client.return_value.init_index.assert_called_once_with()
+        tasks.index_enterprise_catalog_courses_in_algolia_task(
+            content_keys=['fakeX'],
+            algolia_fields=self.ALGOLIA_FIELDS,
+        )
 
         # create expected Algolia data
         expected_algolia_objects = []
@@ -131,26 +177,21 @@ class EnterpriseCatalogCeleryTaskTests(TestCase):
         })
 
         # verify partially_update_index is called with the correct Algolia object data
-        mock_search_client.return_value.partially_update_index.assert_called_once_with(expected_algolia_objects)
+        mock_search_client().partially_update_index.assert_called_once_with(expected_algolia_objects)
 
-    @mock.patch('enterprise_catalog.apps.api.tasks.AlgoliaSearchClient')
+    @mock.patch('enterprise_catalog.apps.api.tasks.get_initialized_algolia_client', return_value=mock.MagicMock())
     def test_index_algolia_with_batched_uuids(self, mock_search_client):
         """
         Assert that the correct data is sent to Algolia index, with the expected enterprise
         catalog and enterprise customer associations.
         """
-        ALGOLIA_FIELDS = ['key', 'objectID', 'enterprise_customer_uuids', 'enterprise_catalog_uuids']
-
         algolia_data = self._set_up_factory_data_for_algolia()
 
         tasks.index_enterprise_catalog_courses_in_algolia_task(
-            ALGOLIA_FIELDS,
             content_keys=['fakeX'],
+            algolia_fields=self.ALGOLIA_FIELDS,
             uuid_batch_size=1,
         )
-
-        # verify Algolia index is initialized
-        mock_search_client.return_value.init_index.assert_called_once_with()
 
         # create expected Algolia data
         expected_algolia_objects = []
@@ -177,4 +218,4 @@ class EnterpriseCatalogCeleryTaskTests(TestCase):
         })
 
         # verify partially_update_index is called with the correct Algolia object data
-        mock_search_client.return_value.partially_update_index.assert_called_once_with(expected_algolia_objects)
+        mock_search_client().partially_update_index.assert_called_once_with(expected_algolia_objects)
