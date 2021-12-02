59086bbce50628bf23752bbddfc0b862a805e5b5
Alexander Sheehan <asheehan@edx.org>
2021-12-02 15:54:53 -0500

    Revert "fix: updated migrations to be fully backwards and forwards compatible"
    This reverts commit f0690c3c82573521858e025f51e3370cfbbc1345.

--- enterprise_catalog/apps/catalog/migrations/0028_auto_20211109_2347.py
+++ enterprise_catalog/apps/catalog/migrations/0028_auto_20211109_2347.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('catalog', '0028_contentmetadatatoqueries'),
+        ('catalog', '0027_contentmetadatatoqueries'),
     ]
 
     operations = [
