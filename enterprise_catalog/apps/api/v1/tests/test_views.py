a68239701ca97fde0f10afe392cac51cda6dffd3
Alexander Sheehan <asheehan@edx.org>
2022-01-08 19:03:07 -0700

    fix: soft delete on query/metadata through table and algolia reindexing job improvements

--- enterprise_catalog/apps/api/v1/tests/test_views.py
+++ enterprise_catalog/apps/api/v1/tests/test_views.py
@@ -26,6 +26,7 @@ from enterprise_catalog.apps.catalog.constants import (
 from enterprise_catalog.apps.catalog.models import (
     CatalogQuery,
     ContentMetadata,
+    ContentMetadataToQueries,
     EnterpriseCatalog,
 )
 from enterprise_catalog.apps.catalog.tests.factories import (
@@ -1024,6 +1025,7 @@ class EnterpriseCustomerViewSetTests(APITestMixin):
     def tearDown(self):
         super().tearDown()
         # clean up any stale test objects
+        ContentMetadataToQueries.all_objects.all().hard_delete()
         CatalogQuery.objects.all().delete()
         ContentMetadata.objects.all().delete()
         EnterpriseCatalog.objects.all().delete()
