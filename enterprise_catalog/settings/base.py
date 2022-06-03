547e9a001e3584f1908cfa6fda8d966e484c539d
Usama Sadiq <usama.sadiq@arbisoft.com>
2022-06-03 18:10:25 +0500

    build: add cookie monitoring middleware (#462)

--- enterprise_catalog/settings/base.py
+++ enterprise_catalog/settings/base.py
@@ -71,6 +71,7 @@ INSTALLED_APPS += THIRD_PARTY_APPS
 INSTALLED_APPS += PROJECT_APPS
 
 MIDDLEWARE = (
+    'edx_django_utils.monitoring.CookieMonitoringMiddleware',
     'edx_django_utils.monitoring.DeploymentMonitoringMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
