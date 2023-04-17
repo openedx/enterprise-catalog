590c7f9e78f98f00e30cfdd965de75fc2891b94e
Brian Beggs <macdiesel@gmail.com>
2023-04-17 08:37:38 -0400

    chore: update yaml loader for safe loading opsgenie alert: #48774

--- enterprise_catalog/settings/production.py
+++ enterprise_catalog/settings/production.py
@@ -23,7 +23,7 @@ FILE_STORAGE_BACKEND = {}
 
 CONFIG_FILE = get_env_setting('ENTERPRISE_CATALOG_CFG')
 with open(CONFIG_FILE, encoding='utf-8') as f:
-    config_from_yaml = yaml.load(f)
+    config_from_yaml = yaml.load(f, Loader=yaml.SafeLoader)
 
     # Remove the items that should be used to update dicts, and apply them separately rather
     # than pumping them into the local vars.
