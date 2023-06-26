2827c92a5f28a9e31a621166e9b636513b82431c
John Nagro <jnagro@edx.org>
2023-06-26 16:41:45 -0400

    feat: adding addition exceptions to retry (#628)

--- enterprise_catalog/apps/api_client/base_oauth_with_retry.py
+++ enterprise_catalog/apps/api_client/base_oauth_with_retry.py
@@ -1,7 +1,22 @@
-from requests.adapters import HTTPAdapter
-from urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter, Retry
+from requests.exceptions import RequestException
 
 from .base_oauth import BaseOAuthClient
+
+
+class EnterpriseRetry(Retry):
+    """
+    A class to tweak which Exceptions are considered retryable by Retry
+    """
+
+    def _is_read_error(self, err: Exception) -> bool:
+        """Errors that occur after the request has been started, so we should
+        assume that the server began processing it.
+        """
+        super_result = super()._is_read_error(err)
+        # this is triggered by a ProtocolError but isnt in the stack
+        local_result = isinstance(err, RequestException)
+        return super_result or local_result
 
 
 class BaseOAuthClientWithRetry(BaseOAuthClient):
@@ -19,7 +34,7 @@ class BaseOAuthClientWithRetry(BaseOAuthClient):
             **kwargs
     ):
         super().__init__(**kwargs)
-        retry = Retry(
+        retry = EnterpriseRetry(
             total=max_retries,
             backoff_factor=backoff_factor,
             backoff_jitter=backoff_jitter,
