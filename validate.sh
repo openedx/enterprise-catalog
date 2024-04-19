0c30a05bea2418e09c816b9cdffb10082b721ea5
Muhammad Umar Khan <m.umarkhan999@gmail.com>
2024-04-19 16:00:36 +0500

    feat: add python 3.12 support

--- validate.sh
+++ validate.sh
@@ -6,7 +6,11 @@ export DJANGO_SETTINGS_MODULE=enterprise_catalog.settings.test
 
 cd /edx/app/enterprise_catalog/enterprise_catalog
 
-make requirements
+if [[ "$TOXENV" == "py38-django42" ]]; then
+    make requirements
+elif [[ "$TOXENV" == "py312-django42" ]]; then
+    make requirements312
+fi
 
 # Alex Dusenbery 2022-04-12: This is failing CI for a reason I don't understand
 # and I don't know why we care about translations here, anyway.
