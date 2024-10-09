9a3deeb4f5e6d1e60e9c9b517a087d9cd85bc5bb
Alexander Dusenbery <adusenbery@2u.com>
2024-10-09 15:51:58 -0400

    fix: fix weird migration
    1. Deletes commented out model field and related commented out unique index.
    2. Changes catalog migration 0039 to not be conditional on the engine, I believe we used this as a hueristic to *not* apply something in stage/prod, but since we use mysql locally to generate migration files, its messing with the makemigrations if we don't change the previous migration.
    3. Introduces a (no-op in terms of SQL) migration to alter the CatalogQuery.content_filter_field. AFAICT, the only real change here is that the JSONEncoder on the field now sets ensure_ascii to False in `dump_kwargs`.

--- enterprise_catalog/apps/catalog/migrations/0039_alter_catalogquery_unique_together_and_more.py
+++ enterprise_catalog/apps/catalog/migrations/0039_alter_catalogquery_unique_together_and_more.py
@@ -8,33 +8,18 @@ class Migration(migrations.Migration):
     dependencies = [
         ('catalog', '0038_alter_catalogquery_unique_together_and_more'),
     ]
-    db_engine = connection.settings_dict['ENGINE']
-    if 'sqlite3' in db_engine:
-        operations = [
-            migrations.AlterUniqueTogether(
-                name='catalogquery',
-                unique_together=set(),
-            ),
-            migrations.AlterField(
-                model_name='catalogquery',
-                name='content_filter_hash',
-                field=models.CharField(editable=False, max_length=32, null=True, unique=True),
-            ),
-            migrations.RemoveField(
-                model_name='catalogquery',
-                name='include_exec_ed_2u_courses',
-            ),
-        ]
-    else:
-        operations = [
-            migrations.AlterField(
-                model_name='catalogquery',
-                name='content_filter_hash',
-                field=models.CharField(editable=False, max_length=32, null=True, unique=True),
-            ),
-            migrations.RemoveField(
-                model_name='catalogquery',
-                name='include_exec_ed_2u_courses',
-            ),
-        ]
-
+    operations = [
+        migrations.AlterUniqueTogether(
+            name='catalogquery',
+            unique_together=set(),
+        ),
+        migrations.AlterField(
+            model_name='catalogquery',
+            name='content_filter_hash',
+            field=models.CharField(editable=False, max_length=32, null=True, unique=True),
+        ),
+        migrations.RemoveField(
+            model_name='catalogquery',
+            name='include_exec_ed_2u_courses',
+        ),
+    ]
