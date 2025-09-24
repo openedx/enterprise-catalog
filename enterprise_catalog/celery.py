8636ddb50466c01771cff0b5b1400a86ce182a9b
Alexander Dusenbery <adusenbery@2u.com>
2025-09-24 12:46:54 -0400

    feat: create worker readiness and liveness files on celery signal

--- enterprise_catalog/celery.py
+++ enterprise_catalog/celery.py
@@ -1,7 +1,36 @@
 """
 Defines the Celery application for the enterprise_catalog project
 """
+import logging
+from pathlib import Path
+
 from celery import Celery
+from celery.signals import heartbeat_sent, worker_ready, worker_shutdown
+
+
+HEARTBEAT_FILE = Path("/tmp/worker_heartbeat")
+READINESS_FILE = Path("/tmp/worker_ready")
+
+
+logger = logging.getLogger(__name__)
+
+
+@worker_ready.connect
+def worker_ready(**_):
+    logger.info('worker_ready signal received, touching the readiness file')
+    READINESS_FILE.touch()
+
+
+@worker_shutdown.connect
+def worker_shutdown(**_):
+    logger.info('worker_shutdown signal received, unlinking readiness and heartbeat files')
+    READINESS_FILE.unlink(missing_ok=True)
+    HEARTBEAT_FILE.unlink(missing_ok=True)
+
+
+@heartbeat_sent.connect
+def heartbeat(**_):
+    HEARTBEAT_FILE.touch()
 
 
 app = Celery('enterprise_catalog', )
